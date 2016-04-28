de939a876d55af0b6e0ba82a4c20ec9fb75618b5
Stacy <stacy.gee@sojern.com>
2016-04-28 11:32:56 -0700

    Import stacy sad files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sad::Application.config.secret_token = '3cd51f5c19ef2811aaeb64b0e1cfef9bed40c0993d8669273c034f7f5ff6c12e5025ddcd119dfea27534af239d31cea262b49d2f588e580fcaba086c2e771226'
